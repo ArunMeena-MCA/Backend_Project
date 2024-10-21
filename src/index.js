0909294ec7636483fa0206bed24ecce03210e3b3
Arun Meena <Arunmeena.mca@gmail.com>
2024-10-21 16:11:26 +0530

    all setup configured

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import express from "express";
+// import express from "express";
 import dotenv from "dotenv"
-import mongoose from "mongoose";
-import {DB_NAME} from "./constants.js";
+// import mongoose from "mongoose";
+// import {DB_NAME} from "./constants.js";
 import connectDB from "./db/index.js";
+import {app} from "./app.js"
 
 dotenv.config({
-    path: './env'
+    path: './.env'
 })
 
-const app = express();
+// const app = express();
 
 connectDB()
 .then(()=>{
